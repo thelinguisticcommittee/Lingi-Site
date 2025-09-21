a0fbff6bc263d0deff80861b18f5996d6cfd629a
thelinguisticcommittee <thelinguisticcommittee@gmail.com>
2025-09-22 00:52:13 +0530

    Sponsers.jsx

--- src/app/components/Sponsers.jsx
+++ src/app/components/Sponsers.jsx
@@ -209,7 +209,7 @@ Your support helps us scale the fest, attract top talent and create memorable ex
           <div className="bg-black/40 border border-purple-700/30 rounded-xl p-6 hover:shadow-purple-500/20 hover:shadow-xl transition transform hover:scale-105 max-w-xs w-full">
             <img
               src="/rakesh sir .jpg"
-              alt="Coordinator"
+              alt="Convener"
               className="w-28 h-28 mx-auto rounded-full mb-4 border-2 border-purple-400 object-cover"
             />
             <h4 className="text-xl font-semibold text-purple-200">
